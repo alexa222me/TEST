e8ad1c5917876ee401885d57ba321d64f3e7c0ff
Alexa Mendoza <alexandra96me@hotmail.com>
2022-11-07 01:04:24 -0600

    Completed Session 2.10

--- TEST/ViewController.swift
+++ TEST/ViewController.swift
@@ -13,14 +13,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
     var imageNumber = 0
+    var messageNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func pressButton(_ sender: UIButton) {
-       
-     print(imageNumber)
+       var messages = ["You Are Awesome!",
+                        "You Are Great!",
+                        "You Are Fantastic!",
+                        "Fabulous? That's You!"]
+        
+        messageLabel.text = messages[messageNumber]
+        messageNumber += 1
+        if messageNumber == messages.count {
+            messageNumber = 0
+        }
+        
         // let imageName = "image" + String(imageNumber)
         let imageName = "image\(imageNumber)"
         imageView.image = UIImage (named: imageName)
